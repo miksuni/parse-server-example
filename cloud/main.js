d261a4018b1548575f79703c00cb3c5f660c6ecf
Mikko <msoft@hotmail.fi>
2018-10-10 21:02:01 +0300

    reverting hello functions

--- cloud/main.js
+++ cloud/main.js
@@ -1,8 +1,8 @@
 
-/*Parse.Cloud.define('hello', function(req, res) {
+Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
-});*/
-
-Parse.Cloud.define(‘hello’, (req) => {
-     return ‘ok’;
 });
+
+/*Parse.Cloud.define(‘hello’, (req) => {
+     return ‘ok’;
+});*/
