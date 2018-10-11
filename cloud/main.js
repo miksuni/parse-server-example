90ecad511750b465e2b54a92f58c8eaad13c6718
Mikko <msoft@hotmail.fi>
2018-10-11 20:22:32 +0300

    yet another try to fix hello 3

--- cloud/main.js
+++ cloud/main.js
@@ -1,9 +1,8 @@
 
-Parse.Cloud.define('hello', function(req, res) {
-  //res.success('Hi');
-  res.error('sjot');
-});
+//Parse.Cloud.define('hello', function(req, res) {
+//  res.success('Hi');
+//});
 
-/*Parse.Cloud.define(‘hello’, (req) => {
-     return ‘ok’;
-});*/
+Parse.Cloud.define('hello', (req) => {
+     return 'ok';
+});
