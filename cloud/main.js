7e3e3a7f5d87fd626fb41cb659d2f228b30c7e83
Mikko <msoft@hotmail.fi>
2018-10-24 19:47:33 +0300

    added test func to hello

--- cloud/main.js
+++ cloud/main.js
@@ -8,7 +8,7 @@ Parse.Cloud.define('hello', (req) => {
     //return 'ok';
     var returnMessage = '';
 
-    var exercises = Parse.Object.extend("Exercise");
+    /*var exercises = Parse.Object.extend("Exercise");
     returnMessage = 'step 1';
     var query = new Parse.Query(exercises);
     returnMessage = 'step 2';
@@ -16,7 +16,7 @@ Parse.Cloud.define('hello', (req) => {
 		returnMessage = 'step 3';
     }).catch(function(error){
 		returnMessage = 'step 4';
-    })
+    })*/
     /*query.find({
 		success: function(results) {
 			console.log("Exercise query ok");
@@ -27,6 +27,21 @@ Parse.Cloud.define('hello', (req) => {
 			returnMessage = 'error';
 		}
 	});*/
+
+	var obj = new Parse.Object('GameScore');
+	returnMessage = 'step 1';
+	obj.set('score',1337);
+	returnMessage = 'step 2';
+	obj.save().then(function(obj) {
+		returnMessage = 'step 3';
+		console.log(obj.toJSON());
+		var query = new Parse.Query('GameScore');
+		returnMessage = 'step 4';
+		query.get(obj.id).then(function(objAgain) {
+			returnMessage = 'step 5';
+			console.log(objAgain.toJSON());
+		}, function(err) {console.log(err); });
+	}, function(err) { console.log(err); });
 
     return returnMessage;
 });
