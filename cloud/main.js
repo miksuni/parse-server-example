7cf77f1da035f7c7bf9247dcf80d9d3a8fb9aaec
Mikko <msoft@hotmail.fi>
2018-10-24 18:44:11 +0300

    added test func to hello

--- cloud/main.js
+++ cloud/main.js
@@ -4,17 +4,21 @@
 //});
 
 Parse.Cloud.define('hello', (req) => {
-	 console.log(req);
-     return 'ok';
+	console.log(req);
+    //return 'ok';
+    var returnMessage = '';
 
-     var exercises = Parse.Object.extend("Exercise");
-     var query = new Parse.Query(exercises);
-     query.find({
+    var exercises = Parse.Object.extend("Exercise");
+    var query = new Parse.Query(exercises);
+    query.find({
 		success: function(results) {
 			console.log("Exercise query ok");
+			returnMessage = 'ok';
 		},
 		error: function(error) {
 			console.log("Exercise query failed: " + error.message);
+			returnMessage = 'error';
 		}
 	});
+    return returnMessage;
 });
