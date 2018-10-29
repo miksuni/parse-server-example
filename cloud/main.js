82b1d7ed85798a15eb0cc97711f223ee9024c96b
Mikko <msoft@hotmail.fi>
2018-10-29 21:24:19 +0200

    added test functionality to settings API func

--- cloud/main.js
+++ cloud/main.js
@@ -67,7 +67,7 @@ Parse.Cloud.define('hello', async (req) => {
 });
 
 Parse.Cloud.define('settings', async (req) => {
-	console.log(">>Request: " + req);
+	console.log(">>Request: " + JSON.stringify(req));
 	//console.log(req.Input);
 
 	/*if (Object.keys(req.Input).length > 0) {
