72736fc819bcb003dc34a9581032602c3123d0ea
Mikko <msoft@hotmail.fi>
2018-11-02 21:57:49 +0200

    added test functionality to settings API func

--- cloud/main.js
+++ cloud/main.js
@@ -67,61 +67,11 @@ Parse.Cloud.define('hello', async (req) => {
 });
 
 Parse.Cloud.define('settings', async (req) => {
-	//console.log(">>Request: " + JSON.stringify(req));
-
-	/*if (Object.keys(req.params).length > 0) {
-		console.log(">> json contains data");
-
-		var newSettings = new Parse.Object('Settings');
-
-		if ('name' in req.params) {
-			console.log('>>' + req.params.name);
-			//newSettings.name = req.params.name;
-			newSettings.set('name', req.params.name);
-			console.log('>>>>' + newSettings.name);
-		} else {console.log('>>> name not found');}
-		if ('exerciseCount' in req.params) {
-			console.log('>>' + req.params.exerciseCount);
-			//newSettings.exerciseCount = req.params.exerciseCount;
-			newSettings.set('exerciseCount', parseInt(req.params.exerciseCount,10));
-			console.log('>>>>' + newSettings.exerciseCount);
-		} else {console.log('>> exerciseCount not found');}
-		if ('pauseInSec' in req.params) {
-			console.log('>>' + req.params.pauseInSec);
-			//newSettings.pauseInSec = req.params.pauseInSec;
-			newSettings.set('pauseInSec', parseInt(req.params.pauseInSec,10));
-			console.log('>>>>' + newSettings.pauseInSec);
-		}
-		if ('repeatsInSet' in req.params) {
-			console.log('>>' + req.params.repeatsInSet);
-			//newSettings.repeatsInSet = req.params.repeatsInSet;
-			newSettings.set('repeatsInSet', parseInt(req.params.repeatsInSet,10));
-			console.log('>>>>' + newSettings.repeatsInSet);
-		}
-
-		newSettings.save().then(function(newSettings) {
-			console.log('>> settings saved');
-		}, function(err) { console.log('>> error in saving: ' + err); });
-		//const results = await newSettings.save();
-		console.log('>> settings saved');
-
-	} else {
-		console.log(">> empty json");
-	}*/
 
 	let returnMessage = 'Ok';
 
-	//var newSettings = results[i];
-	//console.log('>>>>');
-	//console.log('>> result[i]: '+JSON.stringify(results[i]));
-	//console.log('>> newSettings: '+JSON.stringify(newSettings));
-
 	if (Object.keys(req.params).length > 0) {
 		console.log(">> json contains data");
-
-
-
-
 
 		const query = new Parse.Query('Settings');
 		const results = await query.find();
@@ -137,7 +87,6 @@ Parse.Cloud.define('settings', async (req) => {
 			if (n == 0) {break;}
 			i++;
 		}
-
 
 		//var newSettings = new Parse.Object('Settings');
 		var newSettings = results[i];
@@ -192,10 +141,6 @@ Parse.Cloud.define('settings', async (req) => {
 			i++;
 		}
 
-		//returnMessage =  JSON.stringify({name: newSettings.name,
-		//				  exerciseCount: newSettings.exerciseCount,
-		//				  pauseInSec: newSettings.pauseInSec,
-		//				  repeatsInSet: newSettings.repeatsInSet});
 		returnMessage =  JSON.stringify({name: settingsNameVal,
 						  exerciseCount: exerciseCountVal,
 						  pauseInSec: pauseInSecVal,
